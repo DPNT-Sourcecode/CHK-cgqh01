b4daa680a2083b3c4df9c39e8990182b05c1bc42
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 11:16:37 +0000

    Fri Mar 23 11:16:37 UTC 2018

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -3,7 +3,9 @@ package befaster.solutions;
 import befaster.runner.SolutionNotImplementedException;
 
 public class Hello {
+    private static final String GENERIC_HELLO_WORLD = "Hello, World!";
+
     public static String hello(String friendName) {
-        return "Hello " + friendName;
+        return GENERIC_HELLO_WORLD;
     }
 }
--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -5,17 +5,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HelloTest {
+   private static final String GENERIC_HELLO_WORLD = "Hello, World!";
 
     @Test
     public void say_hello_to_someone() {
         String bellaGreeting = Hello.hello("Bella");
-        assertEquals("Hello Bella", bellaGreeting);
+        assertEquals(GENERIC_HELLO_WORLD, bellaGreeting);
     }
-
-//    @Test
-//    public void say_hello_to_nobody() {
-//        String nobody = Hello.hello(null);
-//        assertEquals("Hello Bella", nobody);
-//    }
 
 }
