d387b54fd32bd73577a4cf6bd235dcfca5cd6e06
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 11:10:11 +0000

    Fri Mar 23 11:10:11 UTC 2018

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -8,7 +8,12 @@ import static org.hamcrest.Matchers.equalTo;
 public class SumTest {
 
     @Test
-    public void compute_sum() {
+    public void compute_sum_minimal_values() {
         assertThat(Sum.sum(1, 1), equalTo(2));
+    }
+
+    @Test
+    public void compute_sum_maximum_values () {
+        assertThat(Sum.sum(100, 100), equalTo(200));
     }
 }
