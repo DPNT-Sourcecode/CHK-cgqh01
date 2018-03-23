0e7ffce9aa59a2dff6d86603d70aa73c94c0a82d
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 11:25:25 +0000

    Fri Mar 23 11:25:25 UTC 2018

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
+package befaster.solutions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CheckoutTest {
+
+    @Test
+    public void test_can_buy_a_single_A() {
+        int price = Checkout.checkout("A");
+        assertEquals(50, price);
+    }
 
 }
