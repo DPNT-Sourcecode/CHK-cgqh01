5b83044a73fd7da3617666b36dada6e4c0a447d6
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 11:13:32 +0000

    Fri Mar 23 11:13:32 UTC 2018

---
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -0,0 +1,15 @@
+package befaster.solutions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    public void say_hello_to_someone() {
+        String bellaGreeting = Hello.hello("Bella");
+        assertEquals("Hello Bella", bellaGreeting);
+    }
+
+}
