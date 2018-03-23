63bd080979f4679d0e86f9698f3c5fd12c449289
sbx_user1059 <sbx_user1059@ip-10-12-255-214.eu-west-2.compute.internal>
2018-03-23 12:47:05 +0000

    Fri Mar 23 12:47:05 UTC 2018

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -11,22 +11,36 @@ public class Checkout {
     private static final String DELIMITER = ",";
     private static final int INVALID = -1;
 
-    private static final List<Item> items = Arrays.asList(
-            new Item("A", 50),
-            new Item("B", 30),
-            new Item("C", 20),
-            new Item("D", 15)
-    );
+    private static final Item A = new Item("A", 50);
+    private static final Item B = new Item("B", 30);
+    private static final Item C = new Item("C", 20);
+    private static final Item D = new Item("D", 15);
+
+    private static final List<Item> items = Arrays.asList(A, B, C, D);
 
     private static final Map<String, Item> itemsBySku = items
             .stream()
             .collect(Collectors.toMap(i -> i.sku, i -> i));
 
+    private static final Deal threeAsFor130 = new Deal(A, 3, 130);
+
+    private static final List<Deal> deals = Arrays.asList(threeAsFor130);
+
+    private static final Map<String, List<Deal>> dealsBySku = deals.stream()
+            .collect(Collectors.groupingBy(d -> d.item.sku));
+
     public static Integer checkout(String skus) {
         return Optional.ofNullable(skus)
-                .flatMap(itemSkus -> convertToValidItems(itemSkus)
-                        .map(items -> items.stream().mapToInt(i -> i.priceInWholePounds).sum()))
+                .flatMap(itemSkus -> convertToValidItems(itemSkus).map(Checkout::getPrice))
                 .orElse(INVALID);
+    }
+
+    private static int getPrice(List<Item> items) {
+        Map<String, List<Item>> itemsBySku = items.stream().collect(Collectors.groupingBy(i -> i.sku));
+        itemsBySku.entrySet()
+                .stream()
+                .map((sku, itemsForSku) -> )
+        return items.stream().mapToInt(i -> i.priceInWholePounds).sum();
     }
 
     private static Optional<List<Item>> convertToValidItems(String skus) {
@@ -46,6 +60,18 @@ public class Checkout {
                     return Optional.empty();
                 });
 
+    }
+
+    private static class Deal {
+        private final Item item;
+        private final int quantityToQualifyForDeal;
+        private final int dealPriceInWholePounts;
+
+        private Deal(Item item, int quantityToQualifyForDeal, int dealPriceInWholePounts) {
+            this.item = item;
+            this.quantityToQualifyForDeal = quantityToQualifyForDeal;
+            this.dealPriceInWholePounts = dealPriceInWholePounts;
+        }
     }
 
     private static class Item {
